9edfa9f534f9d3f67814781e4d850a194d4346c0
arthur finamore <arthurfinamore@gmail.com>
2024-02-16 14:39:10 -0300

    add players model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_16_131757) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_16_173608) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "players", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.string "position"
+    t.integer "goals_scored"
+    t.integer "assists"
+    t.integer "dribbles"
+    t.integer "tackles"
+    t.integer "red_card"
+    t.integer "yellow_card"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
